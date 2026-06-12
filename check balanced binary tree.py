9f07f361f0fde1cc0954c901a8cb92adbdc683ce
Mayank <mayankshakya780@gmail.com>
2026-06-12 21:11:01 +0530

    this check balanced binary tree or not

---
+++ check balanced binary tree.py
@@ -0,0 +1,14 @@
+class Solution:
+    def height(self,root):
+        if root is None:
+            return 0
+        return 1+max(self.height(root.left), self.height(root.right))
+    def isBalanced(self,root):
+        if root is None:
+            return True
+        left_height=self.height(root.left)
+        right_height=self.height(root.right)
+        current=abs(left_height-right_height)
+        if current>1:
+            return False
+        return self.isBalanced(root.left)and self.isBalanced(root.right)
