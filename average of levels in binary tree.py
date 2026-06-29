9c9de22d90c44261773b247388e4320132f736eb
Mayank <mayankshakya780@gmail.com>
2026-06-29 20:25:58 +0530

    this give the average of tree of each level

--- average of levels in binary tree.py
+++ average of levels in binary tree.py
@@ -13,11 +13,13 @@ class Solution(object):
             size=len(queue) # har baar queue ka size calculate karnge
             level_sum=0     #fir us level ki sabhi val ko level_sum name ke variable me add kar dange
             for level in range(size):   # fir ik loop chalenge  us level par  queue ke size tak 
-                node=queue.pooleft()    # fir hum queue me store value ko pop(nikal) kar denge aur usko node name ke variable me store kar denge
+                node=queue.popleft()    # fir hum queue me store value ko pop(nikal) kar denge aur usko node name ke variable me store kar denge
                 level_sum+=node.val     # fir level_sum me us node ki sabhi value ko add ka denge
                 if left:                # agar us node ka koi left child hai
-                    queue.append(node,left)     # to uski val ko queue ke dal do
+                    queue.append(node.left)     # to uski val ko queue ke dal do
                 if right:               #same
                     queue.append(node.right)
             result.append(float(level_sum)/size)    #level_sum ki val ko us queue ke size se divid kar do aur usko result me append kar do kyuki average=val/number_of val
         return result
+    
+
